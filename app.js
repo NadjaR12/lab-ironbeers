765238e74a6d04ca570390e78fa051b151003531
Lorelai <nadja.rathenow@rub.de>
2022-02-03 18:39:04 +0100

    done for today

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const { getEnabledCategories } = require('trace_events');
 
 const app = express();
 const punkAPI = new PunkAPIWrapper();
@@ -14,12 +15,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
-
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {doctitle: "Main Page"});
 });
 
+app.get('/beers', (req, res) => {
+punkAPI.getBeers()
+.then(beersFromApi => {
+    res.render('beers', {doctitle: "Beer Page", beers: beersFromApi})
+})
+})
+
+app.get('/random-beer', (req, res) => {
+punkAPI.getRandom()
+  .then(responseFromApi => {
+    res.render('randombeer', {doctitle: "Random Beer", randombeer: responseFromApi})
+  // console.log(responseFromApi)
+})
+})
+
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
